6ba6f85d0075d2989730aa0d6f05113de95e7e68
Venkata-Ramakrishna-Mandalemula <venkatamandalemula@gmail.com>
2024-08-12 18:43:31 -0500

    Updates

---
+++ typingGame/index.js
@@ -0,0 +1,81 @@
+//  (actualWords / totalTimeTaken) * 60;
+
+const typing_ground = document.querySelector('#textarea');
+const btn = document.querySelector('#btn');
+const score = document.querySelector('#score');
+const show_sentence = document.querySelector('#showSentence');
+
+let startTime, endTime, totalTimeTaken;
+
+
+const sentences = ['Typing speed and accuracy improve with consistent practice and focus.',
+    'The rain in Spain stays mainly in the plain.',
+    "I have not failed. I've just found 10,000 ways that won't work.",
+    "Success usually comes to those who are too busy to be looking for it.",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+    "If you can dream it, you can achieve it.",
+    "The most difficult thing in life is to not take risks.",
+    "Every person is born with a talent. What defines them is how they use their talent."];
+
+// step 5
+
+const calculateTypingSpeed = (time_taken) => {
+    let  totalWords = typing_ground.value.trim();
+    let actualWords = totalWords === '' ? 0 : totalWords.split(" ").length;
+
+    if(actualWords !== 0) {
+        let typing_speed  =  (actualWords / time_taken) * 60;
+        typing_speed = Math.round(typing_speed);
+        score.innerHTML = `Your typing speed is ${typing_speed} words per minutes & you wrote ${actualWords} words & time taken ${time_taken} sec`;
+    }else{
+        score.innerHTML = `Your typing speed is 0 words per minutes & time taken ${time_taken} sec`;
+    }
+}
+
+// step 4
+const endTypingTest = () => {
+    btn.innerText = "Start";
+
+    let date = new Date();
+    endTime = date.getTime();
+
+    totalTimeTaken = (endTime -startTime) / 1000;
+
+    // console.log(totalTimeTaken);
+
+    calculateTypingSpeed(totalTimeTaken);
+
+    show_sentence.innerHTML = "";
+    typing_ground.value = "";
+}
+
+
+// step 3
+const startTyping = () => {
+    let randomNumber = Math.floor(Math.random() * sentences.length);
+    // console.log(randomNumber);
+    show_sentence.innerHTML = sentences[randomNumber];
+
+    let date = new Date();
+    startTime = date.getTime();
+
+    btn.innerText = "Done";
+}
+
+
+
+
+// step 2
+btn.addEventListener('click', () => {
+    switch (btn.innerText.toLowerCase()) {
+        case "start":
+            typing_ground.removeAttribute('disabled');
+            startTyping();
+            break;
+
+        case "done":
+            typing_ground.setAttribute('disabled' , 'true');
+            endTypingTest();
+            break;
+    }
+})
